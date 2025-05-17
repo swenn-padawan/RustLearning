3af2b861436a8a125a8c6a181e31ec2fd90c3a4d
Swenn Tetrel <stetrel@2A4.42angouleme.fr>
2025-05-17 10:02:27 +0200

    Rustlings Save (Option2 OK -> 48/94)

--- rustlings/exercises/12_options/options1.rs
+++ rustlings/exercises/12_options/options1.rs
@@ -2,13 +2,15 @@
 // If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
-
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
-    if hour_of_day > 23{
-       return None;
+    if hour_of_day < 22{
+        Some(5)
+    }else if hour_of_day > 23 {
+        None
+    }else {
+        Some(0)
     }
-    return Some(5)
 }
 
 fn main() {
@@ -17,12 +19,13 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::maybe_icecream;
+    use super::*;
+
     #[test]
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
-        
-        let icecreams = maybe_icecream(12);
+        // Option?
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
